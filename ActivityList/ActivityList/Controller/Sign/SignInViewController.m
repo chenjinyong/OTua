a290658e171831dfb52758a59abd719a0a6409ae
zhuyongquan <2240942212@qq.com>
2017-08-23 17:48:57 +0800

    0823 下午六点

--- ActivityList/ActivityList/Controller/Sign/SignInViewController.m
+++ ActivityList/ActivityList/Controller/Sign/SignInViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "SignInViewController.h"
+#import "UserModel.h"
 
 @interface SignInViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passdwordTextField;
+- (IBAction)signInAction:(UIButton *)sender forEvent:(UIEvent *)event;
+
+@property (strong,nonatomic) UIActivityIndicatorView * aiv;
 
 @end
 
@@ -18,6 +24,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self naviConfig];
+    [self uiLayout];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -48,6 +55,16 @@
     //[self.navigationController popViewControllerAnimated:YES];
 }
 
+-(void)uiLayout{
+    //判断是否存在用户名记忆体
+    if ([[Utilities getUserDefaults:@"Username" ]isKindOfClass:[NSNull class]]) {
+        if ([Utilities getUserDefaults:@"Username"] != nil) {
+            //将它显示在用户名输入框中
+            _usernameTextField.text = [Utilities getUserDefaults:@"Username"];
+        }
+    }
+}
+
 
 /*
 #pragma mark - Navigation
@@ -59,4 +76,119 @@
 }
 */
 
+- (IBAction)signInAction:(UIButton *)sender forEvent:(UIEvent *)event {
+    
+    if (_usernameTextField.text.length == 0) {
+        [Utilities popUpAlertViewWithMsg:@"请输入您的手机号" andTitle:nil onView:self];
+        return;
+    }
+    //判断某个字符串中是否每个字符都是数字
+    NSCharacterSet * notDigits = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+    if ([_usernameTextField.text rangeOfCharacterFromSet:notDigits].location != NSNotFound ) {
+        [Utilities popUpAlertViewWithMsg:@"请输入有效的手机号码" andTitle:nil onView:self];
+        return;
+    }
+    if (_passdwordTextField.text.length == 0) {
+        [Utilities popUpAlertViewWithMsg:@"请输入您的密码" andTitle:nil onView:self];
+        return;
+    }
+    if (_passdwordTextField.text.length < 6 || _passdwordTextField.text.length >18) {
+        [Utilities popUpAlertViewWithMsg:@"您输入的密码必须在6-18位之间" andTitle:nil onView:self];
+        return;
+    }
+    //无输入异常的情况下，开始正式登录接口
+    [self readyForencoding];
+    
+}
+
+//登录接口
+- (void)readyForencoding{
+    _aiv = [Utilities getCoverOnView:self.view];
+    
+    [RequestAPI requestURL:@"/login/getKey" withParameters:@{@"deviceType":@7001,@"deviceId": [Utilities uniqueVendor]} andHeader:nil byMethod:kGet andSerializer:kForm success:^(id responseObject) {
+        NSLog(@"responseObject = %@",responseObject);
+        
+        if ([responseObject[@"resultFlag"] integerValue] == 8001) {
+            NSDictionary * result = responseObject[@"result"];
+            NSString * exponent = result[@"exponent"];
+            NSString * modulus = result[@"modulus"];
+            //对内容进行MD5加密
+            NSString * md5Str = [_passdwordTextField.text getMD5_32BitString];
+            //用模数与指数对MD5加密过后的密码进行加密
+            NSString * rsaStr = [NSString encryptWithPublicKeyFromModulusAndExponent:md5Str.UTF8String modulus:modulus exponent:exponent];
+            [self signInWithEncryptPwd:rsaStr];
+            
+        }else{
+            [_aiv stopAnimating];
+            NSString *errorMsg = [ErrorHandler getProperErrorString:[responseObject[@"resultFlag"]integerValue]];
+            [Utilities popUpAlertViewWithMsg:errorMsg andTitle:nil onView:self];
+            
+        }
+    } failure:^(NSInteger statusCode, NSError *error) {
+        [_aiv stopAnimating];
+        [Utilities popUpAlertViewWithMsg:@"请保持网络连接畅通" andTitle:nil onView:self];
+    }];
+
+}
+
+-(void)signInWithEncryptPwd:(NSString * )encryptPwd{
+    [RequestAPI requestURL:@"/login" withParameters:@{@"userName": _usernameTextField.text,@"password": encryptPwd,@"deviceType": @7001,@"deviceId":[Utilities uniqueVendor]} andHeader:nil byMethod:kPost andSerializer:kJson success:^(id responseObject) {
+        
+        [_aiv stopAnimating];
+        NSLog(@"responseObject = %@",responseObject);
+        if ([responseObject[@"resultFlag"] integerValue] == 8001) {
+            NSDictionary * result = responseObject[@"result"];
+            UserModel *user = [[UserModel alloc] initWithDictionary:result];
+            //将登陆获取到的用户信息打包存储到单例化全局变量中
+            [[StorageMgr singletonStorageMgr] addKey:@"MemberInfo" andValue:user];
+            //单独将用户的ID也存储到单例化全局变量来作为用户是否已经登陆的判断一句，同时也方便其他所有页面更快捷的使用用户Id这个参数
+            [[StorageMgr singletonStorageMgr] addKey:@"MemberId" andValue:user.memberId];
+            //如果键盘还打开着就收回去
+            [self.view endEditing:YES];
+            //清空密码输入框里的内容
+            _passdwordTextField.text = @"";
+            //记忆用户名
+            [Utilities setUserDefaults:@"Username" content:_usernameTextField.text];
+            //用Model的方式返回上一页
+            [self dismissViewControllerAnimated:YES completion:nil];
+            
+        }else{
+            
+            NSString *errorMsg = [ErrorHandler getProperErrorString:[responseObject[@"resultFlag"]integerValue]];
+            [Utilities popUpAlertViewWithMsg:errorMsg andTitle:nil onView:self];
+        }
+        
+    } failure:^(NSInteger statusCode, NSError *error) {
+        [_aiv stopAnimating];
+        [Utilities popUpAlertViewWithMsg:@"请保持网络畅通" andTitle:nil onView:self];
+        
+    }];
+}
+
+//按键盘上的return键收起键盘
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+//按键盘外任意部位收起键盘
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
 @end
+
+
+
+
+
+
+
+
+/*
+ if (_usernameText.text.length < 11) {
+ [Utilities popUpAlertViewWithMsg:@"请输入有效的手机号码" andTitle:nil onView:self];
+ return;
+ }
+ */
+
+
